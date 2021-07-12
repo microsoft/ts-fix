ec61a16ddc3d64b0305a26ea6f36b9a9cd337b54
Andrew Branch <andrew@wheream.io>
2021-07-12 14:04:22 -0700

    gitignore dist

--- dist/index.js
+++
@@ -1,8 +0,0 @@
-
-'use strict'
-
-if (process.env.NODE_ENV === 'production') {
-  module.exports = require('./mylib.cjs.production.min.js')
-} else {
-  module.exports = require('./mylib.cjs.development.js')
-}
